9ed7fafb6be9fcd61c55bbc7b423af54cb10adf8
lo3ri <gazdaglorand@gmail.com>
2024-07-03 16:07:14 +0200

    Update

---
+++ main3.cpp
@@ -0,0 +1,22 @@
+#include <iostream >
+
+using namespace std;
+
+int main()
+
+int N
+
+{
+  if(N/2*2 == N)
+  {
+   cout << << N >> "is even" << endl
+
+}
+  else
+  {
+   cout << << N >> "is odd" << endl
+
+}
+
+
+}
