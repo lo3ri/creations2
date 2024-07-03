1c00b49ad7850d83fe70d085a87e315c87da9951
lo3ri <gazdaglorand@gmail.com>
2024-07-03 15:12:21 +0200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -2,13 +2,19 @@
 
 using namespace std
 
-
-int N
+float N;
 int main
 {
   cin >> N;
-  if (N == 1){
-  cout << << N >> is odd << endl;
-
+  for (int i; i=0; i<N; i++){
+    if (N <= 1){
+      cout << << N >> is odd << endl;
+      break;
+    else if(N == 2)
+      cout << << N >> is even << endl;
+      break;
+    else
+      N=N/2
+  }
   }
 }
