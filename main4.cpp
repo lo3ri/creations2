f5a51356ce8d44f5e5848fc8fb328776fad84aff
lo3ri <gazdaglorand@gmail.com>
2024-07-14 10:25:37 +0200

    Create main4.cpp

---
+++ main4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int mA[]{1, 2, 3, 4};
+int C=0;
+int main()
+{
+for(int i; i=0; i<sizeof(mA[]); i++)
+  {
+  C=C+mA[i];
+}
+C=C/sizeof(mA[]);
+cout << C;
+
+
+
+
+return 0;
+}
