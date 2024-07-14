060c83f2d0c698e008101fff12c01916ca9ac676
lo3ri <gazdaglorand@gmail.com>
2024-07-14 16:59:49 +0200

    uj

--- main4.cpp
+++ main4.cpp
@@ -5,18 +5,19 @@ using namespace std;
 
 int main()
 {
-    int mA[4] = {1, 2, 3, 4};
-    float C=0;
-    int sizemA = sizeof(mA) / sizeof(int);
+    int mA[2][4] = {{1, 2, 3, 4} , {200, 100, 150, 200}};
+    float C=0, M=0;
+    int sizemA = sizeof(mA) / sizeof(int)/2;
 
-    cout << sizemA << endl << mA[0] << endl;
+   // cout << sizemA  << endl;
 
     for(int i=0; i< sizemA; i++)
         {
-          C=C+mA[i];
+          C += mA[0][i]*(mA[1][i]/100);
+          M += mA[1][i]/100; 
         }
-    C=C/sizemA;
-    cout << "Átlag: " << C << endl;
+    C /= M;
+    cout << "Súlyozott átlag: " << C << endl;
 
     return 0;
 }
