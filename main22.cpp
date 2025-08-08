23616afa4cd0eec60535a6bbf8b51c9a7facc6bc
lo3ri <gazdaglorand@gmail.com>
2025-08-08 09:18:36 +0200

    Update main22.cpp

--- main22.cpp
+++ main22.cpp
@@ -6,11 +6,15 @@ int main();
 {
  int array[10] = {2,3,5,7,1,4,8,6,0,9};
  int N;
- for (int I, I= 0, I < 10, I++;)
-  array[I]= N;
+ int A=array.size;
+ for (int I= 0, I < A, I++)
+{
+ for (int I= 0, I < A, I++)
+   {N=array[I];
   if (N>array[(I+1)])
    {array[I]= array[(I+1)];
    array[(I+1)]= N;}
- 
+ }
+ }
   return 0;
 }
