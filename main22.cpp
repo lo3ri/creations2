c2fd8c4d8533965f7500810eadbf27cb9ecb1a4c
lo3ri <gazdaglorand@gmail.com>
2025-08-08 09:10:20 +0200

    Create main22.cpp

---
+++ main22.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main();
+{
+ int array[10] = {2,3,5,7,1,4,8,6,0,9};
+ int N;
+ for (int I, I= 0, I < 10, I++;)
+  array[I]= N;
+  if (N>array[(I+1)])
+   {array[I]= array[(I+1)];
+   array[(I+1)]= N;}
+ 
+  return 0;
+}
